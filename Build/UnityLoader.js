b667f0063805d7e7792a4f256f371193f4146b6b
sugarHulk <50421200+sugarHulk@users.noreply.github.com>
2020-02-17 09:25:39 +0100

    Update UnityLoader.js

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -769,19 +769,7 @@ var UnityLoader = UnityLoader || {
 
   })(),
   compatibilityCheck: function (gameInstance, onsuccess, onerror) {
-    if (!UnityLoader.SystemInfo.hasWebGL) {
-      gameInstance.popup("Your browser does not support WebGL",
-        [{text: "OK", callback: onerror}]);
-    } else if (UnityLoader.SystemInfo.mobile) {
-      gameInstance.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.",
-        [{text: "OK", callback: onsuccess}]);
-    } else if (["Edge", "Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1) {
-      gameInstance.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.",
-        [{text: "OK", callback: onsuccess}]);
-    } else {
-      onsuccess();
-    }
-    
+  onsuccess();
   },
   Blobs: {},
   loadCode: function (code, onload, info) {
